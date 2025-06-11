92a52d2803e33a7cbfb5fa336fbd357991636b11
Loona <Loona.com2005@gmail.com>
2025-06-11 19:00:54 +0300

    Fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -194,4 +194,6 @@ def get_top_products():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
